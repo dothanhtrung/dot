3885c231ad7af5fd197f3ada628c68d85d87be44
Trung Do <dothanhtrung@pm.me>
2024-08-02 18:54:33 +0700

    hyprland config

---
+++ .local/bin/hyprlock.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+grim /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+convert /tmp/screen.png ~/.icons/lock.png -gravity center -composite -matte /tmp/screen.png
+#dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
+hyprlock
+rm /tmp/screen.png
