ba0ba1b65ba948af25bde6f6e6237dc897a35e33
Trung Do <dothanhtrung@pm.me>
2025-10-24 02:31:13 +0700

    Add fish config

---
+++ .config/fish/conf.d/aliases.fish
@@ -0,0 +1,23 @@
+abbr less "vim -u ~/.vim/less.vim -"
+abbr more "vim -u ~/.vim/less.vim"
+
+abbr '...' 'cd ../..'
+abbr '....' 'cd ../../..'
+
+abbr hibernate "sudo systemctl hibernate"
+
+# Better alternatives
+abbr cat "bat -p"
+abbr ls exa
+abbr grep rg
+abbr cd z
+abbr fd "fd -I"
+abbr la "ls -a"
+abbr ll "ls -l"
+
+abbr dd "dd status=progress"
+abbr rsync "rsync --progress"
+abbr icat "kitty +kitten icat"
+
+abbr hx "hx --log /tmp/helix.log"
+abbr yt-dlp 'yt-dlp --no-mtime -w --cookies-from-browser firefox::none'
