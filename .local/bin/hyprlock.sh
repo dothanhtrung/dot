b934709da5b195df8dd59ca2e16b1355d3d9dffc
Trung Do <dothanhtrung@pm.me>
2026-02-26 23:53:29 +0700

    Change hyprshot dir

--- .local/bin/hyprlock.sh
+++ .local/bin/hyprlock.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 grim /tmp/screen.png
-convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-convert /tmp/screen.png ~/.icons/lock.png -gravity center -composite -matte /tmp/screen.png
+convert /tmp/screen.png -scale 5% -scale 2000% /tmp/screen.png
+convert /tmp/screen.png ~/.local/share/icons/lock.png -gravity center -composite -matte /tmp/screen.png
 #dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
 hyprlock
 #rm /tmp/screen.png
